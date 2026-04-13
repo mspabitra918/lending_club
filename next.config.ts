476def46a9ec92b7d9d9f32cf4557f4a2eba424e
Pabitra Ghara <pabitraghara384@gmail.com>
2026-04-13 20:18:41 +0530

    /** @type {import('next').NextConfig} */ const nextConfig = {   output: "export", };
    module.exports = nextConfig;

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
