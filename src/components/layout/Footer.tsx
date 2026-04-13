c33546ab02dd22d82039ecd8b9fe554629afdd16
Pabitra Ghara <pabitraghara384@gmail.com>
2026-04-13 20:42:24 +0530

    Refactor : support@lendingfinance.site

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -93,7 +93,7 @@
 //                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
 //                   />
 //                 </svg>
-//                 <span>support@lendingfinance.site</span>
+//                 <span>leads@lendingfinance.site</span>
 //               </div>
 //               <div className="flex items-start gap-3">
 //                 <svg
@@ -274,7 +274,7 @@ export default function Footer() {
                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                   />
                 </svg>
-                <span>support@lendingfinance.site</span>
+                <span>leads@lendingfinance.site</span>
               </div>
               <div className="flex items-start gap-3">
                 <svg
